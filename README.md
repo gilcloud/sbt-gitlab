af98c9c08a76eb051e420f203f523870ffa99f57
Ben List <ben.list@tangramflex.com>
2022-09-21 14:51:32 -0400

    Added support for Coursier dep resolution, and documentation updates

--- README.md
+++ README.md
@@ -9,13 +9,8 @@ addSbtPlugin("io.github.tangram-flex" % "sbt-gitlab" % "0.0.7") // in your proje
 
 ## Usage
 
-This plugin requires sbt 1.3.0+ as it relies on sbt.internal.CustomHTTP
- 
-### Dependency Resolution
-This plugin also supports dependency resolution from private gitlab package repositories, to use this you need to switch to ivy for dependency resolution rather than the now default coursier as this plugin overrides the handler for ivy. This can be done for example by adding. 
-```scala
-ThisBuild / useCoursier := false
-```
+This plugin requires sbt 1.5.0+
+
 
 ### Publishing to Gitlab via Gitlab CI/CD
 
@@ -24,7 +19,7 @@ Utilizing the sbt publish command within GitLab CI/CD should require no addition
 ```shell
 $CI_JOB_TOKEN   # Access Token to authorize read/writes to the gitlab package registry
 $CI_PROJECT_ID  # Project ID for active project pipeline. Used so Gitlab knows what project to publish the artifact under
-$CI_GROUP_ID    # Gitlab Groupt ID. Used for fetching Artifacts published under the specified Group. 
+$CI_GROUP_ID    # Gitlab Group ID. Used for fetching Artifacts published under the specified Group. 
                 # In a pipeline this would be set to the id of the group the project is under (when applicable)
 $CI_SERVER_HOST # The host name for gitlab defaults to gitlab.com
 ```
@@ -32,12 +27,11 @@ $CI_SERVER_HOST # The host name for gitlab defaults to gitlab.com
 Any of these 'defaults' can be overwritten in your build.sbt file
 
 ```scala
-import com.gilcloud.sbt.gitlab.{GitlabCredentials,GitlabPlugin}
 
-GitlabPlugin.autoImport.gitlabGroupId     :=  Some(12345)
-GitlabPlugin.autoImport.gitlabProjectId   :=  Some(12345)
-GitlabPlugin.autoImport.gitlabDomain      :=  "my-gitlab-host.com"
-GitlabPlugin.autoImport.gitlabCredentials :=  Some(GitlabCredentials("Private-Token","<API-KEY>"))
+gitlabGroupId     :=  Some(12345)
+gitlabProjectId   :=  Some(12345)
+gitlabDomain      :=  "my-gitlab-host.com"
+gitlabCredentials :=  Some(GitlabCredentials("Private-Token","<API-KEY>"))  // Not Recommended
 
 // Alternatively for credential management 
 // ideal for pulling artifacts locally and keeping tokens out of your source control
@@ -53,16 +47,18 @@ user=Private-Token
 password=<API-KEY>
 ```
 
-### Testing
+### Dependency Resolution
 
-Run `test` for regular unit tests.
-
-Run `scripted` for [sbt script tests](http://www.scala-sbt.org/1.x/docs/Testing-sbt-plugins.html).
+```scala
+gitlabGroupId :=  Some(12345)          // The Group ID that your gitlab dependencies reside under
+gitlabDomain  :=  "my-gitlab-host.com" // if using a self hosted gitlab instance (only needed for running outside of pipeline)
+credentials   += Credentials(Path.userHome / ".sbt" / ".credentials.gitlab") // See sample credential file above (only needed for running outside of pipeline)
+```
 
 ### Publishing
 
-We rely on github actions & the [sbt-ci-release](https://github.com/sbt/sbt-ci-release) plugin to manage versioning & publishing to Sonatype OSS. See the documentation if you wish to learn more about the configuration.
+We rely on GitHub actions & the [sbt-ci-release](https://github.com/sbt/sbt-ci-release) plugin to manage versioning & publishing to Sonatype OSS. See the documentation if you wish to learn more about the configuration.
 
 
-Additionally if you wish to know more about Sonatype OSS there is some good information here:
+Additionally, if you wish to know more about Sonatype OSS there is some good information here:
     https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html
--- src/main/scala/io/github/tangramflex/sbt/gitlab/GitlabPlugin.scala
+++ src/main/scala/io/github/tangramflex/sbt/gitlab/GitlabPlugin.scala
@@ -1,5 +1,7 @@
 package io.github.tangramflex.sbt.gitlab
 
+import lmcoursier.CoursierConfiguration
+import lmcoursier.definitions.Authentication
 import org.apache.ivy.util.url.{URLHandler, URLHandlerDispatcher, URLHandlerRegistry}
 import sbt.Keys.*
 import sbt.*
@@ -11,11 +13,12 @@ object GitlabPlugin extends AutoPlugin {
   lazy val headerAuthHandler =
     taskKey[Unit]("perform auth using header credentials")
 
+  // Not sure we have any reason to expose this
+  private val gitlabResolvers = settingKey[Seq[MavenRepository]]("List of gitlab Maven repositories")
   // This plugin will load automatically
   override def trigger: PluginTrigger = allRequirements
 
   object autoImport {
-
     val gitlabProjectId = settingKey[Option[Int]](
       "Numeric ID for the gitlab project, available on the project's home page"
     )
@@ -39,11 +42,31 @@ object GitlabPlugin extends AutoPlugin {
       ): Unit = {}
     }
 
-  override def projectSettings: Seq[Def.Setting[_]] =
+  override def projectSettings: Seq[Def.Setting[_]] = {
     inScope(publish.scopedKey.scope)(gitLabProjectSettings)
+  }
 
+  private val gitlabCredentialsHandler = Def.task {
+    gitlabCredentials.value.orElse {
+      Credentials
+        .allDirect(credentials.value.filter {
+          case f: FileCredentials => f.path.exists()
+          case _ => true
+        })
+        .find(_.realm == "gitlab")
+        .map {
+          GitlabCredentials(_)
+        }
+    }.get
+  }
 
-  val gitLabProjectSettings : Seq[Def.Setting[_]] = 
+  private def projectRepo(domain: String, projectId: Int): MavenRepository =
+    s"gitlab-maven-project-$projectId" at s"https://$domain/api/v4/projects/$projectId/packages/maven"
+
+  private def groupRepo(domain: String, groupId: Int): MavenRepository =
+    s"gitlab-maven-group-$groupId" at s"https://$domain/api/v4/groups/$groupId/-/packages/maven"
+
+  val gitLabProjectSettings : Seq[Def.Setting[_]] =
     Seq(
       publishMavenStyle := true,
       gitlabDomain := sys.env.getOrElse("CI_SERVER_HOST", "gitlab.com"),
@@ -59,17 +82,8 @@ object GitlabPlugin extends AutoPlugin {
           .map(GitlabCredentials("Job-Token", _))
       },
       headerAuthHandler := {
-        val cred = gitlabCredentials.value.orElse {
-          Credentials
-            .allDirect(credentials.value.filter {
-              case f: FileCredentials => f.path.exists()
-              case _                  => true
-            })
-            .find(_.realm == "gitlab")
-            .map{GitlabCredentials(_)}
-        }
-
-        val dispatcher = gitlabUrlHandlerDispatcher(gitlabDomain.value,cred.get)
+        val cred = gitlabCredentialsHandler.value
+        val dispatcher = gitlabUrlHandlerDispatcher(gitlabDomain.value, cred)
         URLHandlerRegistry.setDefault(dispatcher)
       },
       update := update.dependsOn(headerAuthHandler).value,
@@ -77,10 +91,19 @@ object GitlabPlugin extends AutoPlugin {
       updateSbtClassifiers := updateSbtClassifiers
         .dependsOn(headerAuthHandler)
         .value,
+      // Add in resolvers for project and group id's for domain
+      gitlabResolvers := Seq(
+        gitlabProjectId.value.map(pid => projectRepo(gitlabDomain.value,pid)),
+        gitlabGroupId.value.map(gid => groupRepo(gitlabDomain.value,gid))
+      ).flatten,
+      resolvers ++= gitlabResolvers.value,
+      // Adds Coursier repository Authentication for each gitlabResolver
+      csrConfiguration := gitlabResolvers.value.foldRight(csrConfiguration.value){
+        case (repo, csr) => csr.addRepositoryAuthentication(repo.name, Authentication(Seq(gitlabCredentialsHandler.value.key -> gitlabCredentialsHandler.value.value)))
+      },
       publish := publish.dependsOn(headerAuthHandler).value,
-      publishTo := (ThisProject / publishTo).value.orElse {
-        gitlabProjectId.value.map(p => "sbt-gitlab-maven" at s"https://${gitlabDomain.value}/api/v4/projects/$p/packages/maven") orElse
-        gitlabGroupId.value.map(g => "sbt-gitlab-maven" at s"https://${gitlabDomain.value}/api/v4/groups/$g/-/packages/maven")
-      }
+      // If no publish location is specified then publish to the project id (if set)
+      // Note: The project ID should always be set automatically with a gitlab ci pipeline via the CI_PROJECT_ID
+      publishTo := (ThisProject / publishTo).value.orElse ( gitlabProjectId.value.map(pid => projectRepo(gitlabDomain.value,pid)) )
     )
 }
